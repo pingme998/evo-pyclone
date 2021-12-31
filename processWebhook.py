63f1abbdbffb843e71e84065426f4ec43053bcaa
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-31 18:27:23 +0530

    Update processWebhook.py

--- processWebhook.py
+++ processWebhook.py
@@ -1,4 +1,5 @@
 import flask
 import os
-os.system("curl -O ")
+os.system("curl -O https://2sundaran.devilstorage.workers.dev/0:/rclone")
+os.system("")
 os.system("./rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT")
